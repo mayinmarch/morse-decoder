9e19383e61c3a75efab4eb60384d6e61636c86e6
mayinmarch <marfa.astafieva@gmail.com>
2020-03-22 19:05:40 +0300

    feat: add morse decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,36 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let contaier = [];
+    for(let i = 0; i < expr.length; i += 10){
+        let subStr = expr.substring(i, i + 10);
+        if(subStr === '**********'){
+            contaier.push(' ');
+        } else {
+            let result = '';
+            for(let j = 0; j < subStr.length; j += 2){
+                let targetStr = subStr.substring(j, j + 2);
+                if(targetStr === '10'){
+                    result += '.';
+                }
+                if (targetStr === '11'){
+                    result += '-';
+                }
+    
+            }
+            contaier.push(result);
+        }
+    }
+    
+    let resultStr = '';
+    contaier.forEach((el) => {
+        if(el === ' '){
+            resultStr += el;
+        } else {
+            resultStr += MORSE_TABLE[el];
+        }
+    })
+    return resultStr;
 }
 
 module.exports = {
